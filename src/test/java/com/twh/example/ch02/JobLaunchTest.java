5ad2131c211671d8429cb54ca6e0bd73b94a990c
tanwenhai <tanwenhai@51jk.com>
2017-10-10 12:35:57 +0800

    添加单元测试

---
+++ src/test/java/com/twh/example/ch02/JobLaunchTest.java
@@ -0,0 +1,29 @@
+package com.twh.example.ch02;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/ch02/job/job.xml")
+public class JobLaunchTest {
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    @Qualifier("billJob")
+    private Job job;
+
+    @Test
+    public void importProducts() throws Exception {
+        JobExecution result = jobLauncher.run(job, new JobParameters());
+        System.out.println(result);
+    }
+}
